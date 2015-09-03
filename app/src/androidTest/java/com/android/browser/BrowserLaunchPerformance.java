06e9c96da67d4f769aacc2364a3adca4369d52f9
Surge1223 <surge1223@gmail.com>
2015-09-03 01:29:46 -0500

    made gradle and studio-able

---
+++ app/src/androidTest/java/com/android/browser/BrowserLaunchPerformance.java
@@ -0,0 +1,27 @@
+package com.android.browser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.common.LaunchPerformanceBase;
+
+public class BrowserLaunchPerformance extends LaunchPerformanceBase {
+
+    @Override
+    public void onCreate(Bundle arguments) {
+        super.onCreate(arguments);
+
+        mIntent.setClassName(getTargetContext(), "com.android.browser.BrowserActivity");
+        start();
+    }
+
+    /**
+     * Calls LaunchApp and finish.
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        LaunchApp();
+        finish(Activity.RESULT_OK, mResults);
+    }
+}
